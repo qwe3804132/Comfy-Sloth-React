748269e8645c376cd124e9f0b9670426cbfcdb35
Peiran Liu <liupeiran9324@outlook.com>
2021-05-13 11:38:57 -0400

    change react back to 16

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -10,12 +10,15 @@ import { useUserContext } from "../context/user_context";
 
 const Sidebar = () => {
   const isOpen = false;
+  const { isSidebarOpen, closeSidebar } = useProductsContext();
   return (
     <SidebarContainer>
-      <aside className={`${isOpen ? "sidebar show-sidebar" : "sidebar"}`}>
+      <aside
+        className={`${isSidebarOpen ? "sidebar show-sidebar" : "sidebar"}`}
+      >
         <div className="sidebar-header">
           <img src={logo} className="logo" alt="Hentai Anime" />
-          <button className="close-btn" type="button">
+          <button className="close-btn" type="button" onClick={closeSidebar}>
             <FaTimes />
           </button>
         </div>
@@ -24,12 +27,16 @@ const Sidebar = () => {
             return (
               <li key={id}>
                 {" "}
-                <Link to={url}>{text}</Link>
+                <Link to={url} onClick={closeSidebar}>
+                  {text}
+                </Link>
               </li>
             );
           })}
           <li>
-            <Link to="/checkout">checkout</Link>
+            <Link to="/checkout" onClick={closeSidebar}>
+              checkout
+            </Link>
           </li>
         </ul>
         <CartButtons />
