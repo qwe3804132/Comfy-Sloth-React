dd80748512b5b08ada62ee4b81b2b5beb0631b20
Peiran Liu <liupeiran9324@outlook.com>
2021-05-12 21:25:13 -0400

    finished Navbar footer and Cart Buttons

--- src/App.js
+++ src/App.js
@@ -1,9 +1,50 @@
-import React from 'react'
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
-import { Navbar, Sidebar, Footer } from './components'
+/* eslint-disable no-unused-vars */
+import React from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { Navbar, Sidebar, Footer } from "./components";
+import {
+  Home,
+  SingleProduct,
+  Cart,
+  Checkout,
+  Error,
+  About,
+  Products,
+  PrivateRoute,
+} from "./pages";
 
 function App() {
-  return <h4>comfy sloth starter</h4>
+  return (
+    <Router>
+      <Navbar />
+      <Sidebar />
+      <Switch>
+        <Route exact path="/">
+          <Home></Home>
+        </Route>
+        <Route exact path="/about">
+          <About></About>
+        </Route>
+        <Route exact path="/cart">
+          <Cart></Cart>
+        </Route>
+        <Route exact path="/products">
+          <Products></Products>
+        </Route>
+        <Route exact path="/products/:id" children={<SingleProduct />}>
+          <Products></Products>
+        </Route>
+        <Route exact path="/checkout">
+          <Checkout></Checkout>
+        </Route>
+        {/* error */}
+        <Route path="*">
+          <Error></Error>
+        </Route>
+      </Switch>
+      <Footer />
+    </Router>
+  );
 }
 
-export default App
+export default App;
